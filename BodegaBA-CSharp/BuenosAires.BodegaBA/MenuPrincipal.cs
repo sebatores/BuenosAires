3bb83ddea7088dca0a10de9109c9344b13dbc260
Eithan <ei.santibanez@duocuc.cl>
2025-05-22 16:43:37 -0400

    menu principal listo (diseño)

--- BodegaBA-CSharp/BuenosAires.BodegaBA/MenuPrincipal.cs
+++ BodegaBA-CSharp/BuenosAires.BodegaBA/MenuPrincipal.cs
@@ -16,5 +16,10 @@ namespace BuenosAires.BodegaBA
         {
             InitializeComponent();
         }
+
+        private void MenuPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- BodegaBA-CSharp/BuenosAires.BodegaBA/ReservasANWO.Designer.cs
+++ BodegaBA-CSharp/BuenosAires.BodegaBA/ReservasANWO.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Buenos Aires";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(484, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sistema de Bodega - Menú Principal";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 64);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Consultar Productos en Bodega";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 64);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Administrar Guías de Despacho";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(255, 295);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 64);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Reservar Equipos de ANWO";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(255, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(213, 64);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Salir";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // ReservasANWO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(722, 471);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ReservasANWO";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
